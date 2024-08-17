10f9f2883e255218231a30a2712ef37dec885f5f
PabloPGRCondori <tsukuraishimotlo@gmail.com>
2024-08-16 19:41:25 -0500

    Creacion de la clase abstracta PecesAcuario y modificacion completa del main

--- src/acuario/main.kt
+++ src/acuario/main.kt
@@ -1,16 +1,13 @@
 package acuario
 
-fun construirAcuario() {
-    val acuario1 = Acuario(largo = 25, ancho = 25, alto = 40)
-    acuario1.imprimirTamano()
+fun crearPeces() {
+    val tiburon = Tiburon()
+    val pezPayaso = PezPayaso()
 
-    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
-    miAcuario.imprimirTamano()
-
-    val miTorre = TanqueTorre(alto = 40, diametro = 25)
-    miTorre.imprimirTamano()
+    println("El color del tiburon es: ${tiburon.color}")
+    println("El color del Pez Payaso es: ${pezPayaso.color}")
 }
 
 fun main() {
-    construirAcuario()
+    crearPeces()
 }
