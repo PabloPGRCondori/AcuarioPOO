c142854af11d8602afa23def7a680b38c13e1fb0
PabloPGRCondori <tsukuraishimotlo@gmail.com>
2024-08-16 19:21:14 -0500

    Cambios hechos, arreglado el var nuevamente, y ahora otra vez

--- src/acuario/main.kt
+++ src/acuario/main.kt
@@ -1,18 +1,16 @@
 package acuario
 
 fun construirAcuario() {
-    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
-    acuario8.imprimirTamano()
+    val acuario1 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario1.imprimirTamano()
 
     val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
     miAcuario.imprimirTamano()
 
-    val miTorre = Acuario.TanqueTorre(diametro = 25, alto = 40)
+    val miTorre = TanqueTorre(alto = 40, diametro = 25)
     miTorre.imprimirTamano()
-
 }
 
 fun main() {
     construirAcuario()
 }
-
