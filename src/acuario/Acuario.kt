1749825c38e779d2040529c9db166b9d40c09fd4
PabloPGRCondori <tsukuraishimotlo@gmail.com>
2024-08-16 19:15:09 -0500

    Cambios hechos, arreglado el var

--- src/acuario/Acuario.kt
+++ src/acuario/Acuario.kt
@@ -1,27 +1,18 @@
-package acuario
 import kotlin.math.PI
 
-open class Acuario (open var largo: Int = 100, open var ancho: Int = 20, open var alto: Int = 40) {
+open class Acuario (
+    open var largo: Int = 100,
+    open var ancho: Int = 20,
+    open var alto: Int = 40
+) {
     open var volumen: Int
         get() = ancho * alto * largo / 1000
         set(valor) {
             alto = (valor * 1000) / (ancho * largo)
         }
 
-    class TanqueTorre (override var alto: Int, var diametro: Int): Acuario(alto = alto, ancho = diametro, largo = diametro) {
-        override var volumen: Int
-            // área elíptica = π * r1 * r2
-            get() = (ancho/2 * largo/2 * alto / 1000 * PI).toInt()
-            set(valor) {
-                alto = ((valor * 1000 / PI) / (ancho/2 * largo/2)).toInt()
-            }
-
-        override var agua = volumen * 0.8
-        override val forma = "cilindro"
-    }
-
     open val forma = "rectángulo"
-    open var agua: Double = 0.0
+    open val agua: Double
         get() = volumen * 0.9
 
     // Código adicional aquí
@@ -32,8 +23,21 @@ open class Acuario (open var largo: Int = 100, open var ancho: Int = 20, open va
                 "Alto: $alto cm ")
         println("Volumen: $volumen l Agua: $agua l (${agua/volumen*100.0}% lleno)")
     }
+}
 
+class TanqueTorre (
+    override var alto: Int,
+    var diametro: Int
+) : Acuario(alto = alto, ancho = diametro, largo = diametro) {
+    override var volumen: Int
+        get() = (ancho / 2 * largo / 2 * alto / 1000 * PI).toInt()
+        set(valor) {
+            alto = ((valor * 1000 / PI) / (ancho / 2 * largo / 2)).toInt()
+        }
 
+    override val forma = "cilindro"
+    override val agua: Double
+        get() = volumen * 0.8
 }
 
 
